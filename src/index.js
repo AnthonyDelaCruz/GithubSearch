140b8f0bd16151177b8c5f0a6c492d45076ed53f
Tony dela cruz <delacruztony.adc@gmail.com>
2020-09-03 23:41:24 +0800

    added react snapshot.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubUsersProvider } from "./context";
+import { render } from "react-snapshot";
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <GithubUsersProvider>
       <App />
